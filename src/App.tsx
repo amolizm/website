f1cf6516877f07014767bc03ddbb777214a7a58d
amol <kambar.amol.m@gmail.com>
2024-05-15 12:37:50 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='heading'>Amol Kambar</h1>
+      <div>
+      </div>
+      <p className="page-content">
+        Hi !
+        Welcome to my website.
+      </p>
+    </>
+  )
+}
+
+export default App
